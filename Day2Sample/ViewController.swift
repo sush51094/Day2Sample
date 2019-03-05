3c4ac95282bd58011a97a4a500d74f982753aa9c
MacStudent <macstudent@126-iMac12.local>
2019-03-05 18:23:53 -0500

    Initial Commit

---
+++ Day2Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Day2Sample
+//
+//  Created by MacStudent on 2019-03-05.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
