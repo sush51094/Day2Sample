3f08161263669885e0ed51567c82a9052549cc4a
MacStudent <macstudent@126-iMac12.local>
2019-03-05 20:50:34 -0500

    changes added

---
+++ Day2Sample/ImageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ImageViewController.swift
+//  Day2Sample
+//
+//  Created by MacStudent on 2019-03-05.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
